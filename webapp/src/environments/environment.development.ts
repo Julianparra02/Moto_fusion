17eb28d4c93ba5f806ec7da0de20d1884ee5b793
Julian Parra <juldav22@hotmail.com>
2025-12-06 14:45:25 -0500

    Fix: change port to 8888 for Railway

--- webapp/src/environments/environment.development.ts
+++ webapp/src/environments/environment.development.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    apiUrl:"http://localhost:3000"
+    apiUrl:"https://motofusion-production.up.railway.app"
 };
--- webapp/src/environments/environment.ts
+++ webapp/src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-      apiUrl:"http://myshop.test"
+      apiUrl:" https://motofusion-production.up.railway.app"
 };
